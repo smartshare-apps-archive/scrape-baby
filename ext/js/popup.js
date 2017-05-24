864419de4ab9865f6048f26ae443bbe04261d844
luke lombardi <llom2600@localhost.localdomain>
2017-05-24 11:06:02 -0400

    transfer push

--- ext/js/popup.js
+++ ext/js/popup.js
@@ -2,15 +2,17 @@ var select_siteToConfigure;
 
 var btn_setParameters;
 var btn_startScrape;
+var btn_gatherFields;
 
 var input_siteParameters;
 var input_siteToScrape;
+var input_siteToTest;
+
 var input_testURL;
 
 var currentURL;
 
-
-
+//https://www.darice.com/store/search?s=perler&p=1&ps=60&o=0
 
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 	console.log(message);
@@ -36,27 +38,45 @@ $(document).ready(function(){
 function bindElements(){
 	btn_setParameters = $("#btn_setParameters");
 	btn_startScrape = $("#btn_startScrape");
+	btn_gatherFields = $("#btn_gatherFields");
 
 	select_siteToConfigure = $("#select_siteToConfigure");
 
 	input_siteParameters = $("#input_siteParameters");
 	input_siteToScrape = $("#input_siteToScrape");
 	input_testURL = $("#input_testURL");
+	input_siteToTest = $("#input_siteToTest");
 }
+
 
 function bindEvents(){
 	btn_setParameters.click(setParameters);
 	btn_startScrape.click(initScrape);
+	btn_gatherFields.click(gatherFields);
 
 	select_siteToConfigure.change(loadParameters);
+}
+
+
+
+function initScrape(){
+	var currentParams = null;
+	//chrome.runtime.sendMessage({params: test_params_b});
 
 }
 
 
-function initScrape(){
-var currentParams = null;
-//chrome.runtime.sendMessage({params: test_params_b});
-
+function gatherFields(){
+	var testURL = input_testURL.val();
+	var currentParams = input_siteToTest.val();
+	
+	chrome.storage.sync.get(currentParams, function (obj) {
+		var paramString = obj[currentParams];
+	 	paramString = paramString.replace('param_url', "\"" + testURL + "\"");
+	 	console.log(paramString);
+	 	var paramObj = JSON.parse("{" + paramString + "}");
+	 	console.log(paramObj); 
+	});
 }
 
 
@@ -79,13 +99,14 @@ function setParameters(){
 
 
    chrome.storage.sync.set(json_obj, function() {
-      console.log('Saved website scrape parameters.');
+       console.log('Saved website scrape parameters.');
    });
 }
 
 
 function loadParameters(){
 	var currentSite = select_siteToConfigure.val();
+	
 	chrome.storage.sync.get(currentSite, function (obj) {
 	   input_siteParameters.val(obj[currentSite]);
 	});
--- ext/js/scape_json.js
+++ ext/js/scape_json.js
@@ -3,7 +3,7 @@ var test_params_a = {
 		"init": {
 				type: "raw",
 				method: "get",
-				url: "https://www.darice.com/store/search?s=perler&p=1&ps=60&o=0",
+				url: param_url,
 				next_action: "get_product_links",
 				requires_session: true,
 				session_params: {
@@ -18,17 +18,16 @@ var test_params_a = {
 				},
 				
    			},
-   		//match_descriptors come in pairs in two
+   		
    		"get_product_links":{
 			type: "parse",
 			search_re_list: 
 				[	
-				{
+					{
 					id: "product_links",
 					exp: String.raw`\"boldLink\sproductLink\"\shref=\"([^\"]+)\"\>([^\<]+)\</a\>`,
 					operation: "iterate",
-					//match_descriptors: ["product_link","product_title"]
-				}
+					}
 				],
 			next_action:"get_product_specs"
 			
@@ -50,12 +49,13 @@ var test_params_a = {
 			type: "parse",
 			search_re_list: 
 				[	
-				{
+					{
 					id: "product_specs",
 					associate_with_key: "true",
 					exp: String.raw`\<div\sclass=\"specRow\"\>[^\<]+\<span\sclass=\"alignLeft\"\>[^A-Za-z0-9\$\.\-]+([A-Za-z0-9\$\s\.\-]+)[^\<]+\</span\>[^\<]+\<span\sclass=\"alignRight\"\>[^A-Za-z0-9\$\-\.]+([A-Za-z0-9\$\s\.\-]+)\</span\>`,
 					operation: "iterate",
-				}
+					}
+
 				],
 			next_action:null
 	   	},
@@ -64,31 +64,32 @@ var test_params_a = {
 
 var test_params_b = {
 		"init": {
+
 				type: "raw",
 				method: "get",
 				url: "https://www.aliexpress.com/item/2014-Mens-Boy-Body-Compression-Base-Layer-Sleeveless-Sport-Vest-Thermal-Under-Top-Tees-Tank-Tops/1918374005.html?ws_ab_test=searchweb0_0,searchweb201602_2_10152_10065_10151_10130_10068_10136_10137_10157_10060_10138_10155_10062_10156_437_10154_10056_10055_10054_10059_303_100032_9872_100033_100031_10099_10103_10102_10096_10147_10052_10053_10050_10107_10142_10051_10084_10083_10080_10082_10081_10178_10110_519_10111_10112_10113_10114_10181_10183_10182_10185_10078_10079_10073_10123_142-9872,searchweb201603_1,ppcSwitch_7&btsid=a530e854-caa9-4608-88f4-d2cc28e1c5a2&algo_expid=433cdc7f-33e5-4419-b039-12b59cdb26b6-2&algo_pvid=433cdc7f-33e5-4419-b039-12b59cdb26b6",
 				next_action: "first_action",
 				requires_session: false
 				
-   			},
+   				},
 
    		"first_action":{
 			type: "parse",
 			search_re_list: 
 				[	
-				{
+					{
 					id: "product_price",
 					exp: String.raw`\<span\sclass=\"p-symbol\"\>(US)\s\$\</span\>\<span\sid=\"j\-sku\-price\"\sclass=\"p\-price\"\>([^\<]+)`,
 					operation: "search",
 					//match_descriptors: ["product_link","product_title"]
-				},
+					},
 
-				{
+					{
 					id: "product_title",
 					exp: String.raw`\<span\sclass=\"p-symbol\"\>(US)\s\$\</span\>\<span\sid=\"j\-sku\-price\"\sclass=\"p\-price\"\>([^\<]+)`,
 					operation: "search",
 					//match_descriptors: ["product_link","product_title"]
-				}
+					}
 				],
 	   	}
   
